7323da0b84ff31d555aca3cbe2db19842423e124
armada <rui.armada@siemens.com>
2024-08-20 19:47:42 +0100

    delete

--- main.tf
+++ main.tf
@@ -11,12 +11,3 @@ provider "aws" {
   region  = "us-west-2"
   
 }
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
